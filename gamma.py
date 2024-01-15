3107c9be1322665e673256bc9a838cb4f30f7b41
sc00termcgavin <suspiciously.unknown@pm.me>
2024-01-14 17:54:35 -0700

    total gamma exposure plot

--- gamma.py
+++ gamma.py
@@ -3,10 +3,12 @@ import numpy as np
 import scipy
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-import datetime
-from datetime import timedelta, date, datetime
-import calendar 
 
+from datetime import timedelta, date, datetime
+
+
+
+pd.options.display.float_format = '{:,.4f}'.format
 filename = 'spx_quotedata.csv'
 """
 Black Scholes with mertons formula accounting for dividens
@@ -34,7 +36,7 @@ def Gamma( S, K, T, r, q, sigma, oType, OI):
         gamma = np.exp(-q*T) * norm.pdf(d1) / (S*sigma*np.sqrt(T))
         return (OI*100*S**2*0.01*gamma)
     else:
-        gamma = K * np.exp(-r*T) * norm.pdf(d2) / (S * S * vol * np.sqrt(T))
+        gamma = K * np.exp(-r*T) * norm.pdf(d2) / (S * S * sigma * np.sqrt(T))
         return OI * 100 * S * S * 0.01 * gamma 
 
 """
@@ -50,3 +52,67 @@ def is_third_friday(s):
 print (is_third_friday('Jan 19, 2024'))  # True
 print (is_third_friday('Feb 16, 2024'))  # True
 print (is_third_friday('Jan 12, 2024'))  # False
+
+"""
+CBOE file import
+"""
+
+# This assumes the CBOE file format hasn't been edited, i.e. table beginds at line 4
+optionsFile = open(filename)
+optionsFileData = optionsFile.readlines()
+optionsFile.close()
+
+# Get SPX Spot
+spotLine = optionsFileData[1]
+spotPrice = float(spotLine.split('Last:')[1].split(',')[0])
+fromStrike = 0.8 * spotPrice
+toStrike = 1.2 * spotPrice
+
+# Get Today's Date
+dateLine = optionsFileData[2]
+todayDate = dateLine.split('Date: ')[1].split(',')
+monthDay = todayDate[0].split(' ')
+
+
+# Get index Options Data
+df = pd.read_csv(filename, sep=",", header=None, skiprows=4)
+df.columns = ['ExpirationDate','Calls','CallLastSale','CallNet','CallBid','CallAsk','CallVol',
+              'CallIV','CallDelta','CallGamma','CallOpenInt','StrikePrice','Puts','PutLastSale',
+              'PutNet','PutBid','PutAsk','PutVol','PutIV','PutDelta','PutGamma','PutOpenInt']
+
+df['ExpirationDate'] = pd.to_datetime(df['ExpirationDate'], format='%a %b %d %Y')
+df['ExpirationDate'] = df['ExpirationDate'] + timedelta(hours=16)
+df['StrikePrice'] = df['StrikePrice'].astype(float)
+df['CallIV'] = df['CallIV'].astype(float)
+df['PutIV'] = df['PutIV'].astype(float)
+df['CallGamma'] = df['CallGamma'].astype(float)
+df['PutGamma'] = df['PutGamma'].astype(float)
+df['CallOpenInt'] = df['CallOpenInt'].astype(float)
+df['PutOpenInt'] = df['PutOpenInt'].astype(float)
+
+"""
+
+
+CALCULATE SPOT GAMMA 
+Gamma Exposure = Unit Gamma * Open Interest * Contract Size * Spot Price 
+To further convert into 'per 1% move' quantity, multiply by 1% of spotPrice
+"""
+df['CallGEX'] = df['CallGamma'] * df['CallOpenInt'] * 100 * spotPrice * spotPrice * 0.01
+df['PutGEX'] = df['PutGamma'] * df['PutOpenInt'] * 100 * spotPrice * spotPrice * 0.01 * -1
+
+df['TotalGamma'] = (df.CallGEX + df.PutGEX) / 10**9
+dfAgg = df.groupby(['StrikePrice']).sum(numeric_only=True)
+strikes = dfAgg.index.values
+
+
+# Chart 1: Absolute Gamma Exposure
+plt.grid()
+plt.bar(strikes, dfAgg['TotalGamma'].to_numpy(), width=10, linewidth=0.1, edgecolor='k', label="Gamma Exposure")
+plt.xlim([fromStrike, toStrike])
+chartTitle = "Total Gamma: $" + str("{:.2f}".format(df['TotalGamma'].sum())) + " Bn per 1% SPX Move"
+plt.title(chartTitle, fontweight="bold", fontsize=20)
+plt.xlabel('Strike', fontweight="bold")
+plt.ylabel('Spot Gamma Exposure ($ billions/1% move)', fontweight="bold")
+plt.axvline(x=spotPrice, color='r', lw=1, label="SPX Spot: " + str("{:,.0f}".format(spotPrice)))
+plt.legend()
+plt.show()
